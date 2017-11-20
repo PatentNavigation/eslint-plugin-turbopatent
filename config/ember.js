1740247731264dcb63b9ef79f209a9f8eebd0030
Claudia Cuevas <omordn@gmail.com>
2017-11-20 13:37:59 -0800

    update base.js & ember.js configuration Move ember rules configuration from base.js -> ember.js Extend from eslint-plugin-ember/recommended-rules

--- config/ember.js
+++ config/ember.js
@@ -1,9 +1,18 @@
 module.exports = {
-  extends: require.resolve('./base.js'),
+  extends: [
+    'plugin:ember/recommended',
+    require.resolve('./base.js')
+  ],
   env: {
     'browser': true
   },
   rules: {
-    'new-cap': [ 'error', { 'capIsNewExceptions': [ 'A' ] } ]
+    'new-cap': [ 'error', { 'capIsNewExceptions': [ 'A' ] } ],
+
+    // Custom rules
+    'ember/avoid-leaking-state-in-components': 'error',
+    'ember/local-modules': 'error',
+    'ember/no-empty-attrs': 'error',
+    'ember/no-global-jquery': 'off'
   }
 };
